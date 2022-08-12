780f80d4f7ffef436897bb89c30a38d1148eab98
f4d0 <f4d0n1@gmail.com>
2022-08-12 12:47:30 +0100

    Debug mode

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -1,9 +1,49 @@
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <stdio.h>
 
 int Error(const char* msg) {
 	printf("%s | (%u)\n", msg, ::GetLastError());
 	return 1;
+}
+
+BOOLEAN SetProcessPrivilege(_In_ HANDLE ProcessHandle, _In_ LPCWSTR PrivilegeName, _In_ BOOLEAN EnablePrivilege, _In_opt_ PBOOLEAN IsEnabled)
+{
+	BOOLEAN Result = FALSE;
+
+	HANDLE TokenHandle = 0;
+
+	if (OpenProcessToken(ProcessHandle, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &TokenHandle))
+	{
+		LUID Luid = { 0 };
+
+		if (LookupPrivilegeValueW(NULL, PrivilegeName, &Luid))
+		{
+			TOKEN_PRIVILEGES PreviousState = { 0 }, NewState = { 0 };
+
+			NewState.PrivilegeCount = 1;
+			NewState.Privileges[0].Luid = Luid;
+			NewState.Privileges[0].Attributes = EnablePrivilege ? SE_PRIVILEGE_ENABLED : 0;
+
+			DWORD ReturnLength = 0;
+
+			Result = AdjustTokenPrivileges(TokenHandle, FALSE, &NewState, sizeof(TOKEN_PRIVILEGES), &PreviousState, &ReturnLength);
+
+			if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+			{
+				SetLastError(ERROR_PRIVILEGE_NOT_HELD);
+				Result = FALSE;
+			}
+			else if (Result && IsEnabled)
+			{
+				if (PreviousState.PrivilegeCount == 0)
+					*IsEnabled = EnablePrivilege;
+				else
+					*IsEnabled = (PreviousState.Privileges[0].Attributes & SE_PRIVILEGE_ENABLED) ? TRUE : FALSE;
+			}
+		}
+		CloseHandle(TokenHandle);
+	}
+	return Result;
 }
 
 int main(int argc, const char* argv[]) {
@@ -13,6 +53,11 @@ int main(int argc, const char* argv[]) {
 		printf("Usage: injector <pid> <dllpath>\n");
 		return 0;
 	}
+
+	if (SetProcessPrivilege(GetCurrentProcess(), L"SeDebugPrivilege", TRUE, NULL)) // Нужны права админа, иначе ошибка ERROR_PRIVILEGE_NOT_HELD
+		wprintf_s(L"Successfully!\n");
+	else
+		wprintf_s(L"Error setting Debug Priviledge! Try in Administrator mode.\n");
 
 	// Open a handle to the target process
 	HANDLE hProcess = OpenProcess(
